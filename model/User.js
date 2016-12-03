6aed000c83363f10912a08e443208a23d5d8b975
Simon Turner <simonturner34@gmail.com>
2016-12-03 16:10:33 +0000

    added group schema and some linking

--- model/User.js
+++ model/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var bcrypt = require('mongoose-bcrypt');
+var Schema = mongoose.Schema;
 
 var userSchema = mongoose.Schema({
     email: { type: String, required: true, index: { unique: true } },
@@ -7,7 +8,8 @@ var userSchema = mongoose.Schema({
     likes: [{ like: String }],
     hates: [{ hate: String }],
     address: String,
-    dietRequirements: [{ requirement: String }]
+    dietRequirements: [{ requirement: String }],
+    groups: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
 });
 
 userSchema.plugin(bcrypt);
--- model/db.js
+++ model/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var bcrypt = require('mongoose-bcrypt');
+var findOrCreate = require('mongoose-findorcreate');
 var User = require('./User.js');
+var Group = require('./Group.js');
 
 mongoose.connect('mongodb://localhost/secret-santa');
 var db = mongoose.connection;
@@ -8,15 +10,23 @@ var db = mongoose.connection;
 //     console.log("test");
 // });
 
-var testUser = User.create({
-    email: 'test@test.com',
-    password: 'test'
-});
+// var testGroup = Group.create({
+//     name: 'Test Group',
+// });
 
-var userQuery = User.findOne({ email: 'test@test.com' });
-userQuery.select('email password');
-userQuery.exec(function(err, user){
-    if(!err) {
-        console.log(user.email);
+
+
+var testGroup = Group.findOrCreate({ name: 'Test Group' },
+    function(err, group, created) {
+        User.create({
+            email: 'test@test.com',
+            password: 'test',
+            groups: [group._id]
+        }, function(err, user){
+            var userQuery = User.findOne({ email: 'test@test.com' }, function(err, user){ console.log(user.groups); });
+        });
     }
-});
+);
+// console.log(testGroup._id)
+
+// console.log(testUser.groups);
